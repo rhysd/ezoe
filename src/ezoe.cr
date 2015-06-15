1fb1ef0d3322410cd1b5e4fcc92976f65d4b22f4
rhysd <lin90162@yahoo.co.jp>
2015-06-16 03:18:31 +0900

    implement 'ezoe' command

---
+++ src/ezoe.cr
@@ -0,0 +1,54 @@
+require "colorize"
+require "option_parser"
+require "http/client"
+require "xml/xml"
+require "cgi"
+
+def error(msg)
+  STDERR.puts msg
+  exit 1
+end
+
+def show_questions(user)
+  response = HTTP::Client.get "http://ask.fm/feed/profile/#{user}.rss"
+  error response.status_message unless response.status_code == 200
+
+  XML.parse(response.body).content.try &.split(/\n\s+\n/)[2..-1].each do |item|
+    question,answer,time,url = item.split('\n').map(&.strip)
+    answer = answer.gsub(/質問ではない。?|不?自由/){|m| m.colorize.green}
+    puts "#{url}\n  #{question.colorize.red}\n  #{answer.colorize.white}\n\n"
+  end
+end
+
+def post_question(user, question)
+  response = HTTP::Client.get "http://ask.fm/#{user}"
+
+  # XXX: Dirty because Crystal doesn't have HTML parser yet
+  token = response.body.match(/<input name="authenticity_token" type="hidden" value="([^"]+)"/).try &.[1]
+  error "authentication token not found" unless token
+
+  params = [
+              "authenticity_token", token,
+              "question[question_text]", question,
+              "question[force_anonymous]", "force_anonymous",
+           ].map{|p| CGI.escape p}.each_slice(2).map{|kv| "#{kv[0]}=#{kv[1]}"}.join('&')
+
+  response = HTTP::Client.post(
+      "http://ask.fm/#{user}/questions/create?#{params}",
+      HTTP::Headers.new.add("Referer", "http://ask.fm/#{user}")
+    )
+  error response.status_message unless response.status_code == 200
+end
+
+user = "EzoeRyou"
+OptionParser.parse(ARGV) do |parser|
+  parser.on("-u USER", "who do you want to ask to?") do |u|
+    user = u
+  end
+end
+
+if ARGV.empty?
+  show_questions(user)
+else
+  post_question(user, ARGV.first)
+end
