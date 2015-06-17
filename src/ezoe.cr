6d8ca64df1585ec8c9707ffb8a45d69345610b4a
rhysd <lin90162@yahoo.co.jp>
2015-06-18 00:51:40 +0900

    some refactorings
    - Type annotation (comment)
    - use `abort`
    - use hash-like construction for HTTP::Headers

--- src/ezoe.cr
+++ src/ezoe.cr
@@ -4,17 +4,17 @@ require "http/client"
 require "xml/xml"
 require "cgi"
 
-def error(msg)
-  STDERR.puts msg
-  exit 1
-end
-
+# Note:
+# I can add type annotations like below:
+#
+#   def show_questions(user : String) : Nil
+#
 def show_questions(user)
   response = HTTP::Client.get "http://ask.fm/feed/profile/#{user}.rss"
-  error response.status_message unless response.status_code == 200
+  abort response.status_message unless response.status_code == 200
 
   XML.parse(response.body).content.try &.split(/\n\s+\n/)[2..-1].each do |item|
-    question,answer,time,url = item.split('\n').map(&.strip)
+    question,answer,time,url = item.split('\n').map(&.strip) # &. is to_proc operator: http://crystal-lang.org/2013/09/15/to-proc.html
     answer = answer.gsub(/質問ではない。?|不?自由/){|m| m.colorize.green}
     puts "#{url}\n  #{question.colorize.red}\n  #{answer.colorize.white}\n\n"
   end
@@ -25,26 +25,37 @@ def post_question(user, question)
 
   # XXX: Dirty because Crystal doesn't have HTML parser yet
   token = response.body.match(/<input name="authenticity_token" type="hidden" value="([^"]+)"/).try &.[1]
-  error "authentication token not found" unless token
+  abort "authentication token not found" unless token
 
   params = CGI.build_form do |form|
     form.add "authenticity_token", token
     form.add "question[question_text]", question
-    form.add "question[force_anonymous]", "force_anonymous",
+    form.add "question[force_anonymous]", "force_anonymous"
   end
 
   response = HTTP::Client.post(
       "http://ask.fm/#{user}/questions/create?#{params}",
-      HTTP::Headers.new.add("Referer", "http://ask.fm/#{user}")
+      HTTP::Headers{"Referer": "http://ask.fm/#{user}"}, # Hash-like types: http://crystal-lang.org/docs/syntax_and_semantics/literals/hash.html
     )
-  error response.status_message unless response.status_code == 200
+  abort response.status_message unless response.status_code == 200
 end
 
 user = "EzoeRyou"
-OptionParser.parse(ARGV) do |parser|
-  parser.on("-u USER", "who do you want to ask to?") do |u|
-    user = u
+begin
+  OptionParser.parse(ARGV) do |parser|
+    parser.banner = "Usage: ezoe [-u {user}] [{question}]"
+
+    parser.on("-u USER", "--user", "who do you want to ask to?") do |u|
+      user = u
+    end
+
+    parser.on("-h", "--help", "Show this help") do
+      puts parser
+      exit 0
+    end
   end
+rescue e : OptionParser::InvalidOption
+  abort e.message
 end
 
 if ARGV.empty?
